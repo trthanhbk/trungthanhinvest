9f0cb15985a78cbc7f573c95f3b1a94d68dbb09c
thanhnt <thanhnt@nadia.bz>
2015-03-24 20:22:36 +0700

    change website color

--- app/views/frontends/lien_he.blade.php
+++ app/views/frontends/lien_he.blade.php
@@ -23,7 +23,7 @@
                     <hr />
                     <i class="icon-envelope-alt"></i> &nbsp; <a href="mailto:trungthanhinan@gmail.com">trungthanhinan@gmail.com</a>
                     <hr />
-                    <img src=" {{ url('asset/frontend/img/website.png')  }}" height="15px"> &nbsp; <a href="http://trungthanhinvest.com.vn">trungthanhinvest.com.vn</a>
+                    <img src=" {{ url('asset/frontend/img/website.png')  }}" height="15px"> &nbsp; <a href="http://trungthanhinvest.com.vn" style="color: blue;">trungthanhinvest.com.vn</a>
                 </div>            
         </div>
     </div>
