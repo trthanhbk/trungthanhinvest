912c1559f05e9425ad23f1510630aea1240ffc02
ThanhNT <thanhnt@nadia.bz>
2015-09-02 12:58:29 +0700

    Add analytics

--- app/views/layouts/frontend-float.blade.php
+++ app/views/layouts/frontend-float.blade.php
@@ -1,6 +1,6 @@
-@include("layouts.elements.displays.header-float")          
-        
-        
+@include("layouts.elements.displays.header-float")
+
+
     @include("layouts.elements.displays.menu-float")
 
     <!-- Begin Body -->
@@ -9,16 +9,16 @@
             <div class="row">
                     <div class="col-sm-3">
                         @include("layouts.elements.displays.sidemenu-float")
-                    </div>  
+                    </div>
                     <div class="col-sm-9">
                         @if(Session::has('message'))
                         <div class="error">
                             {{ Session::get('message') }}
                         </div>
                     @endif
-                    @yield('content')                       
-                       
-                    </div> 
+                    @yield('content')
+
+                    </div>
                     <div class="clearfix"></div>
             </div>
         </div>
@@ -43,15 +43,31 @@
                   offset: {
                     top: $('header').height()
                   }
-            }); 
+            });
 
             $('.float-side').affix({
                   offset: {
                     top: 200
                   }
-            }); 
+            });
         });
     </script>
     @yield('scripts')
+    <!-- Piwik -->
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//www.trungthanhinvest.com.vn/analytics/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', 2]);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//www.trungthanhinvest.com.vn/analytics/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+    <!-- End Piwik Code -->
+
 </body>
 </html>
--- app/views/layouts/frontend.blade.php
+++ app/views/layouts/frontend.blade.php
@@ -38,6 +38,21 @@
 {{ HTML::script('asset/frontend/js/custom.js') }}
 
 @yield('scripts')
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//www.trungthanhinvest.com.vn/analytics/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 2]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//www.trungthanhinvest.com.vn/analytics/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 
 </body>
 </html>
